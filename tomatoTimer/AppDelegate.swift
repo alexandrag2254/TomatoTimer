96bd556c82933b04d98e85584efcfb40b1aef3a5
Alex <alexandrag2254@gmail.com>
2015-08-21 07:02:17 -0700

    Initial Commit

---
+++ tomatoTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tomatoTimer
+//
+//  Created by Jie on 8/21/15.
+//  Copyright © 2015 Jie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
